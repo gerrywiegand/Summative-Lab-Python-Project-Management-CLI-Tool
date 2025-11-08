190dcafc1291335874d7ca9cf25ff7fa3fdf035f
Gerry <gerrywiegand@gmail.com>
2025-11-08 17:12:37 -0500

    minor cleanup, lining utils with main

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .utils import add_user, add_project, add_task
+from .utils import add_project, add_task, add_user
 
-__all__ = ['add_user', 'add_project', 'add_task']
+__all__ = ["add_user", "add_project", "add_task"]
