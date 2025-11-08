9766ef685939f1651bdd8835e9bf90a843098063
Gerry <gerrywiegand@gmail.com>
2025-11-08 16:37:18 -0500

    added properties and setters to users

--- models/projects.py
+++ models/projects.py
@@ -6,3 +6,24 @@ class Project:
         self.due_date = due_date
         Project.all.append(self)
         self.tasks = []
+
+    @property
+    def tasks(self):
+        return self._tasks
+
+    @tasks.setter
+    def tasks(self, tasks):
+        self._tasks = tasks
+        for task in tasks:
+            task.project = self
+
+    @property
+    def users(self):
+        users = set()
+        for task in self.tasks:
+            if task.assigned_to:
+                users.add(task.assigned_to)
+        return list(users)
+
+    def add_task(self, task):
+        task.project = self
--- models/users.py
+++ models/users.py
@@ -6,3 +6,50 @@ class User:
         self.email = email
         User.all.append(self)
         self.projects = []
+
+    @property
+    def tasks(self):
+        tasks = []
+        for project in self.projects:
+            for task in project.tasks:
+                if task.assigned_to == self:
+                    tasks.append(task)
+        return tasks
+
+    @tasks.setter
+    def tasks(self, tasks):
+        for task in tasks:
+            task.assigned_to = self
+            if task.project and self not in task.project.users:
+                task.project.users.append(self)
+
+    @property
+    def projects(self):
+        projects = set()
+        for task in self.tasks:
+            if task.project:
+                projects.add(task.project)
+        return list(projects)
+
+    @projects.setter
+    def projects(self, projects):
+        for project in projects:
+            if self not in project.users:
+                project.users.append(self)
+            for task in project.tasks:
+                if task.assigned_to == self:
+                    if task not in self.tasks:
+                        self.tasks.append(task)
+
+    def add_task(self, task):
+        task.assigned_to = self
+        if task.project and self not in task.project.users:
+            task.project.users.append(self)
+
+    def add_project(self, project):
+        if self not in project.users:
+            project.users.append(self)
+        for task in project.tasks:
+            if task.assigned_to == self:
+                if task not in self.tasks:
+                    self.tasks.append(task)
