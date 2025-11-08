aec38b4c6043ccc7b45ff34615d263fda11c54fe
Gerry <gerrywiegand@gmail.com>
2025-11-08 16:20:44 -0500

    added parsers for project

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ from models.projects import Project  # noqa: F401
 from models.tasks import Task  # noqa: F401
 
 parser = argparse.ArgumentParser(description="Project Management CLI Tool")
-subparses = parser.add_subparsers()
+subparsers = parser.add_subparsers()
+
+add_parser = subparsers.add_parser("add-user", help="Add a new user")
+add_parser.add_argument("username", type=str, help="Username of the user")
+add_parser.add_argument("email", type=str, help="Email of the user")
+
+add_parser = subparsers.add_parser("add-project", help="Add a new project")
+add_parser.add_argument("title", type=str, help="Title of the project")
+add_parser.add_argument("due_date", type=str, help="Due date of the project")
